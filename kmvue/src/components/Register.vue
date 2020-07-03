4c7596008155d120a5f682699cca963f9e5476fa
xianyu <zhang1905263329@163.com>
2020-07-03 18:18:20 +0800

    注册界面完善
    # Conflicts:
    #	kmvue/src/components/cardDetail/cardDetail.vue
    #	kmvue/src/components/cardEdit/CardEdit.vue

--- kmvue/src/components/Register.vue
+++ kmvue/src/components/Register.vue
@@ -1,54 +1,80 @@
 
 <template>
   <body id="poster" style="z-index: 10">
-<el-form :model="rules" :rules="rules" ref="ruleForm"  label-position="left" label-width="100px" class="login-container">
-  <el-form-item   label="用户名" prop="name">
-    <el-input placeholder="请输入用户名" v-model="registerForm.usename"></el-input>
-  </el-form-item>
-  <el-form-item  label="密码" prop="password">
-    <el-input show-password placeholder="请输入密码" v-model="registerForm.password"></el-input>
-  </el-form-item>
-  <el-form-item label="确认密码" prop="confirmpassword">
-    <el-input show-password placeholder="请再次输入密码"  v-model="registerForm.confirmpassword"></el-input>
-  </el-form-item>
-  <el-form-item  label="邮箱" prop="email">
-    <el-input placeholder="请输入email" v-model="registerForm.email"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-row>
-      <el-button onclick="window.location.href ='/login'">返回登录</el-button>
-      <el-button type="primary"   v-on:click="register" >立即创建</el-button>
-    </el-row>
-  </el-form-item>
-</el-form>
+  <el-form :model="registerForm" :rules="rules" ref="registerForm"  label-position="left" label-width="100px" class="login-container">
+    <el-form-item   label="用户名" prop="usename">
+      <el-input placeholder="请输入用户名" v-model="registerForm.usename"></el-input>
+    </el-form-item>
+    <el-form-item  label="密码" prop="password">
+      <el-input show-password placeholder="请输入密码" v-model="registerForm.password"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="confirmpassword">
+      <el-input show-password placeholder="请再次输入密码"  v-model="registerForm.confirmpassword"></el-input>
+    </el-form-item>
+    <el-form-item  label="邮箱" prop="email">
+      <el-input placeholder="请输入email" v-model="registerForm.email"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-row>
+        <el-button onclick="window.location.href ='/login'">返回登录</el-button>
+        <el-button type="primary"   v-on:click="register" >立即创建</el-button>
+      </el-row>
+    </el-form-item>
+  </el-form>
   </body>
 </template>
 <script>
   export default {
     name:'Register',
     data() {
+
+      var check_confirmpassword = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.registerForm.password) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+
+      var checkEmail = (rule, value, callback) => {
+        const mailReg = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+/
+        if (!value) {
+          return callback(new Error('邮箱不能为空'))
+        }
+        setTimeout(() => {
+          if (mailReg.test(value)) {
+            callback()
+          } else {
+            callback(new Error('请输入正确的邮箱格式'))
+          }
+        }, 100)
+      }
       return {
         registerForm: {
           id:'',
           usename: '',
           password: '',
           confirmpassword: '',
-          email: '',
+          email: ''
         },
         rules: {
-          name: [
+          usename: [
             { required: true, message: '请输入用户名', trigger: 'blur' },
             { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
           ],
           password: [
             { required: true, message: '请输入密码', trigger: 'blur' },
-            { min: 3, max: 5, message: '长度6~8，不能有特殊字符如&|', trigger: 'blur' }
+            { min: 6, max: 8, message: '长度6~8，不能有特殊字符如&|', trigger: 'blur' }
           ],
           confirmpassword: [
-            { required: true, message: '请再次输入密码', trigger: 'blur' }
+            {validator: check_confirmpassword, trigger: 'blur' },
+            { required: true, message: '请再次输入密码', trigger: 'blur' },
+            { min: 6, max: 8, message: '长度6~8，不能有特殊字符如&|', trigger: 'blur' }
           ],
           email: [
-            { required: true, message: '请输入邮箱', trigger: 'blur' }
+            { validator: checkEmail, trigger: 'blur' }
           ]
         }
       }
@@ -75,7 +101,7 @@
         var that = this;
         this.$axios
           .post('/register', {
-            // id:this.registerForm.id,
+           // id:this.registerForm.id,
             name: this.registerForm.usename,
             token:'',
             gmtCreate:'',
@@ -87,7 +113,7 @@
           })
           .then(function (res) {
             console.log(res);
-             that.open();
+            that.open();
           })
           .catch(function (errro) {
 
